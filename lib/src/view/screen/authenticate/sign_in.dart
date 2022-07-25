127408f527a026b5b27d4bf818330d85590279cc
Cong-The <88782485+Cong-The@users.noreply.github.com>
2022-07-25 13:57:55 +0700

    init

--- lib/src/view/screen/authenticate/sign_in.dart
+++ lib/src/view/screen/authenticate/sign_in.dart
@@ -28,150 +28,162 @@ class _SignInState extends State<SignIn> {
         : Scaffold(
             backgroundColor: Colors.brown[100],
             body: Container(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 50.0),
-                child: Form(
-                    key: _formKey,
-                    child: SingleChildScrollView(
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Column(
-                          children: <Widget>[
-                            const SizedBox(height: 40),
-                            const Text(
-                              "Welcome",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Colors.brown,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-
-                            //
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                hintText: 'Email',
-                                floatingLabelBehavior:
-                                    FloatingLabelBehavior.always,
-                                suffixIcon: const CustomSurffixIcon(
-                                    svgIcon: "assets/icons/Mail.svg"),
-                              ),
-                              validator: (String? value) {
-                                if (value != null && value.isEmpty) {
-                                  return "Email can't be empty";
-                                }
-                                return null;
-                              },
-                              onChanged: (value) {
-                                setState(() {
-                                  email = value;
-                                });
-                              },
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                hintText: 'Password',
-                                floatingLabelBehavior:
-                                    FloatingLabelBehavior.always,
-                                suffixIcon: const CustomSurffixIcon(
-                                    svgIcon: "assets/icons/Lock.svg"),
-                              ),
-                              validator: (String? value) {
-                                if (value!.length < 6) {
-                                  return "Enter a password 6+ chars long";
-                                }
-                                return null;
-                              },
-                              obscureText: true,
-                              onChanged: (value) {
-                                setState(() {
-                                  password = value;
-                                });
-                              },
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            ElevatedButton(
-                                onPressed: () async {
-                                  if (_formKey.currentState!.validate()) {
-                                    setState(() {
-                                      loading = true;
-                                    });
-                                    dynamic result =
-                                        await _auth.signInWithEmailAndPassword(
-                                            email, password);
-                                    if (result == null) {
-                                      setState(() {
-                                        loading = false;
-                                        error =
-                                            'cloud not sign in with those cedentials';
-                                      });
-                                    }
-                                  }
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.pink[400], // background
-                                  onPrimary:
-                                      Colors.white, // foreground // mau chu
-                                ),
-                                child: const Text(
-                                  'Sign in',
-                                  // style: TextStyle(color: Colors.white),
-                                )),
-
-                            TextButton(
-                                onPressed: () {
-                                  widget.toggleView();
-                                },
-                                style: TextButton.styleFrom(
-                                  primary: Colors.black54,
-                                  textStyle: const TextStyle(
-                                      fontStyle: FontStyle.italic,
-                                      fontSize: 14),
-                                ),
-                                child: const Text(
-                                  'Create accout',
-                                  textAlign: TextAlign.center,
-                                )),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Text(
-                              error,
-                              style: const TextStyle(
-                                  color: Colors.red, fontSize: 14),
-                            ),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SocalCard(
-                                  icon: "assets/icons/google-icon.svg",
-                                  press: () {},
-                                ),
-                                SocalCard(
-                                  icon: "assets/icons/facebook-2.svg",
-                                  press: () {},
-                                ),
-                                SocalCard(
-                                  icon: "assets/icons/twitter.svg",
-                                  press: () {},
-                                ),
-                              ],
-                            ),
-                          ],
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 40),
+                        const Text(
+                          "Welcome",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ))));
+                        //
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        _textfieldEmail(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        _textFieldPassword(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        _buttonSignIn(),
+                        _buttonCreateAccout(),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          error,
+                          style:
+                              const TextStyle(color: Colors.red, fontSize: 14),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        _buttonServiceLogin(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
+
+  Widget _textfieldEmail() {
+    return TextFormField(
+      decoration: textInputDecoration.copyWith(
+        hintText: 'Email',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: const CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+      ),
+      validator: (String? value) {
+        if (value != null && value.isEmpty) {
+          return "Email can't be empty";
+        }
+        return null;
+      },
+      onChanged: (value) {
+        setState(() {
+          email = value;
+        });
+      },
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return TextFormField(
+      decoration: textInputDecoration.copyWith(
+        hintText: 'Password',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: const CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
+      ),
+      validator: (String? value) {
+        if (value!.length < 6) {
+          return "Enter a password 6+ chars long";
+        }
+        return null;
+      },
+      obscureText: true,
+      onChanged: (value) {
+        setState(() {
+          password = value;
+        });
+      },
+    );
+  }
+
+  Widget _buttonSignIn() {
+    return ElevatedButton(
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            setState(() {
+              loading = true;
+            });
+            dynamic result =
+                await _auth.signInWithEmailAndPassword(email, password);
+            if (result == null) {
+              setState(() {
+                loading = false;
+                error = 'cloud not sign in with those cedentials';
+              });
+            }
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          primary: Colors.pink[400], // background
+          onPrimary: Colors.white, // foreground // mau chu
+        ),
+        child: const Text(
+          'Sign in',
+          // style: TextStyle(color: Colors.white),
+        ));
+  }
+
+  Widget _buttonCreateAccout() {
+    return TextButton(
+        onPressed: () {
+          widget.toggleView();
+        },
+        style: TextButton.styleFrom(
+          primary: Colors.black54,
+          textStyle: const TextStyle(fontStyle: FontStyle.italic, fontSize: 14),
+        ),
+        child: const Text(
+          'Create accout',
+          textAlign: TextAlign.center,
+        ));
+  }
+
+  Widget _buttonServiceLogin() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SocalCard(
+          icon: "assets/icons/google-icon.svg",
+          press: () {},
+        ),
+        SocalCard(
+          icon: "assets/icons/facebook-2.svg",
+          press: () {},
+        ),
+        SocalCard(
+          icon: "assets/icons/twitter.svg",
+          press: () {},
+        ),
+      ],
+    );
   }
 }
